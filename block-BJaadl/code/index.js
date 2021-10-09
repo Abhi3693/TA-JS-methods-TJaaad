f55a0e9f26b229f006a79b5bee7acb3fd755491c
Abhijeet Mane <abhi@Varshas-MacBook-Air.local>
2021-10-09 15:06:26 +0530

    added change Assignment level 2 of Array and string method

--- block-BJaadl/code/index.js
+++ block-BJaadl/code/index.js
@@ -27,42 +27,63 @@ console.log(wordToLength);
 
 // - Create a new array that only contains word with atleast one vowel.
 
-let wordWithVowel = words.filter((value) => { return value.includes("a") || value.includes("e") || value.includes("i") || value.includes("o") || value.includes("u") });
-console.log(wordWithVowel);
+function wordWithVowel(value)  { 
+    return (
+    value.includes("a") || 
+    value.includes("e") || 
+    value.includes("i") || 
+    value.includes("o") || 
+    value.includes("u")
+    )
+ };
+
+ let vovelFilter = words.filter((w) => wordWithVowel(w))
+console.log(vovelFilter);
 
 
 // - Find the index of the word "rhythm"
-console.log(words.indexOf("rhythm"));
-
+console.log(words.findIndex((val) => val == "rhythm"));
 
 // - Create a new array that contians words not starting with vowel.
 
- function wordStartWithAplphabet(value)  {
-  if (value.startsWith("a") || value.startsWith("e") || value.startsWith("i") || value.startsWith("o") || value.startsWith("u")) {
-    return false;
-  } else {
-    return value;
-  }
-}
+//  function wordStartWithAplphabet(value)  {
+//   if (value.startsWith("a") || value.startsWith("e") || value.startsWith("i") || value.startsWith("o") || value.startsWith("u")) {
+//     return false;
+//   } else {
+//     return value;
+//   }
+// }
 
-let wordStartAlphabet = words.filter(wordStartWithAplphabet);
-console.log(wordStartAlphabet);
+// let wordStartAlphabet = words.filter(wordStartWithAplphabet);
+// console.log(wordStartAlphabet);
+
+let notWithVowel = words.filter(
+  (w) => !wordWithVowel(w[0])
+  );
+
+  console.log(notWithVowel)
 
 
+  
 // - Create a new array that contianse words not ending with vowel
 
-function wordEndWithAplphabet(value)  {
-  if (value.endsWith("a") || value.endsWith("e") || value.endsWith("i") || value.endsWith("o") || value.endsWith("u") || value.endsWith("a")) {
-    return false;
-  } else {
-    return value;
-  }
-}
+// function wordEndWithAplphabet(value)  {
+//   if (value.endsWith("a") || value.endsWith("e") || value.endsWith("i") || value.endsWith("o") || value.endsWith("u") || value.endsWith("a")) {
+//     return false;
+//   } else {
+//     return value;
+//   }
+// }
 
-let wordEndAlphabet = words.filter(wordEndWithAplphabet);
-console.log(wordEndAlphabet);
+// let wordEndAlphabet = words.filter(wordEndWithAplphabet);
+// console.log(wordEndAlphabet);
 
 
+let notEndingWithVowel = words.filter(
+  (w) => !wordWithVowel(w[w.length - 1])
+  );
+
+  console.log(notEndingWithVowel);
 
 
 
@@ -77,15 +98,22 @@ let numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
 // - Create a sumArray function that takes an array of number as a parameter, and calculate the sum of all its numbers
 
-let findSum = (prev, curr) =>  prev + curr;
-let sum = numbers.reduce(findSum, 0)
-console.log(sum);
+// let findSum = (prev, curr) =>  prev + curr;
+// let sum = numbers.reduce(findSum, 0)
+// console.log(sum);
+
+
+function sumArray (array) {
+  return array.reduce((prev, curr) => {
+    return prev + curr;
+  },0)
+}
 
 
 // - Make a new array that contains number multiplied by 3 like [6, 18, 27 ...]
 
-let multiplyByThree = numbers.map((value) => {
-  return value * 3;
+let multiplyByThree = numbers.filter((value) => {
+  return value % 3 === 0;
 });
 console.log(multiplyByThree);
 
@@ -108,22 +136,14 @@ console.log(odd);
 
 // - Create a new array that should have true for even number and false for odd numbers.
 
-let findOddAndEven = numbers.map((value) => {
-  if (value % 2 === 0){
-    return true;
-  } else {
-    return false;
-  }
-});
+let findOddAndEven = numbers.map((value) => value % 2 === 0);
 console.log(findOddAndEven);
 
 
 // - Sort the above number in assending order.
 
-let newNumbers = numbers.sort((a,b) => {
-  return a - b;
-})
-console.log(newNumbers);
+let sortedArray = [...numbers].sort((a,b) => a - b);
+console.log(sortedArray);
 
 
 // - Does sort mutate the original array?
@@ -133,23 +153,22 @@ console.log(newNumbers);
 
 // - Find the sum of the numbers in the array.
 
-let findSumofArray = (prev, curr) => prev + curr;
-let sumOfNumbers = numbers.reduce(findSum,0);
-console.log(sumOfNumbers);
-
+let sum = numbers.reduce((prev, curr) => {
+  prev = prev + curr;
+  return prev;
+},0)
+console.log(sum);
 
 //- Write a function averageNumbers that receives an array of numbers and calculate the average of the numbers
 
-let findsum = (prev,curr) => {
-  return prev + curr;
+function averageNumbers(array) {
+  let avg = array.reduce((prev, curr) => {
+    prev = prev + curr;
+    return prev;
+  },0) 
+
+  return avg / numbers.length
 }
-
-let avgNumbers = numbers.reduce(findsum) / numbers.length;
-console.log(avgNumbers);
-
-
-
-
 
 
 
@@ -174,8 +193,19 @@ let strings = [
 // - Write a function averageWordLength that receives an array of words2 and calculate the average length of the words.
 
 
-let allString = (prev, curr) => {
-  return prev + curr.length
+// let allString = (prev, curr) => {
+//   prev =  prev + curr.length;
+//   return prev;
+// }
+// let avgString = strings.reduce(allString, 0) / strings.length;
+// console.log(avgString);
+
+
+function avgOfString(words) {
+  return (
+     words.map((val) => val.length).reduce((prev, curr) => {
+      prev = prev + curr;
+      return prev;
+    },0) / strings.length
+  )
 }
-let avgString = strings.reduce(allString, 0) / strings.length;
-console.log(avgString);
